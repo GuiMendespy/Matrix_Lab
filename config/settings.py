cd2b1804241535fb6ce14bf04823daec8594d3e8
David-Mdrs <medeiros.david.ac@gmail.com>
2026-05-22 20:12:18 -0300

    feat: Garante seguranca da chave API

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,22 @@
 import os
 
-# Definicão das chaves de API
-OPENROUTER_API_KEY = "CHAVE AQUI"
+from dotenv import load_dotenv
 
-# Definição dos caminhos para os arquivos
-QUESTION_BANK_PATH = os.getenv("QUESTION_BANK_PATH", "rag/question_bank")
-THEORY_BANK_PATH = os.getenv("THEORY_BANK_PATH", "rag/theory_bank")
+load_dotenv()
+
+# Definicão da chave API (chave real deve ser armazenada em um arquivo .env)
+OPENROUTER_API_KEY = os.getenv(
+    "OPENROUTER_API_KEY"
+)
+
+# Definição do caminho para banco de questões
+QUESTION_BANK_PATH = os.getenv(
+    "QUESTION_BANK_PATH",
+    "rag/question_bank"
+)
+
+# Definição do caminho para banco de teoria
+THEORY_BANK_PATH = os.getenv(
+    "THEORY_BANK_PATH",
+    "rag/theory_bank"
+)
